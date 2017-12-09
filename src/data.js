6a85fa421ab45328cc5811a86fe569d503f6a3d3
Tejas Bubane <tejasbubane@gmail.com>
2017-12-10 00:26:51 +0530

    Use res.json to send json data
    Also module exports - keep data in separate file

---
+++ src/data.js
@@ -0,0 +1,11 @@
+exports.users = [
+  {id: 1, first_name: "Tejas", last_name: "Bubane"},
+  {id: 2, first_name: "Mahesh", last_name: "M."}
+]
+
+exports.products = [
+  {id: 1, name: "Dell XPS", price: 46999 },
+  {id: 2, name: "HP SL-129", price: 35699 },
+  {id: 3, name: "Macbook Air", price: 75999 },
+  {id: 4, name: "Macbook Pro", price: 129999 }
+]
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 const express = require("express")
 const app = express()
+const data = require('./data')
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello World...")
+})
+
+app.get("/users", (req, res) => {
+  res.json(data.users)
+})
+
+app.get("/products", (req, res) => {
+  res.json(data.products)
 })
 
 app.listen(3000, () => console.log("Listening on port 3000..."))
