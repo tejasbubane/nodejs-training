cb92cbf455e79f831999ee933b046b0c6fcab51d
Tejas Bubane <tejasbubane@gmail.com>
2017-12-10 01:17:22 +0530

    Route and query parameters

--- src/index.js
+++ src/index.js
@@ -10,8 +10,25 @@ app.get("/users", (req, res) => {
   res.json(data.users)
 })
 
+// Route params
+// Find user with given id
+app.get("/users/:id", (req, res) => {
+  let user = data.users.find(u => u.id === parseInt(req.params.id));
+  res.json(user);
+})
+
+// Query params
+// Filter products by price range
 app.get("/products", (req, res) => {
-  res.json(data.products)
+  let products = data.products,
+      { min_price, max_price } = req.query;
+  if(min_price && max_price) {
+    products = products.filter(
+      product => product.price >= parseInt(min_price) && product.price <= parseInt(max_price)
+    )
+  }
+
+  res.json(products)
 })
 
 app.listen(3000, () => console.log("Listening on port 3000..."))
