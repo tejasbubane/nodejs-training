5a61783f29090362bfb1891f988fb2f9157809a2
Tejas Bubane <tejasbubane@gmail.com>
2017-12-14 09:34:03 +0530

    Add validations for User model and a user create route

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-  id: Number,
-  first_name: String,
-  last_name: String
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: String,
+  email: {
+    type: String,
+    required: true, 
+    match: [/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, "Invalid email"]
+  }
 })
 
 module.exports = mongoose.model("User", userSchema)
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,14 +4,30 @@ const router = express.Router()
 const mongoose = require("mongoose")
 const User = mongoose.model("User")
 
-// This is a mini app - kinda like Rails engine mounted on `/users`
+let index = (req, res) => {
+  User.find().then(users => res.json(users))
+}
+
+let show = (req, res) => {
+  User.findOne({id: req.params.id}).then(user => res.json(user))
+}
+
+let create = (req, res) => {
+  let userParams = req.body.user
+
+  let user = new User({
+    first_name: userParams.first_name,
+    last_name: userParams.last_name,
+    email: userParams.email
+  })
+  user.save()
+    .then(user => res.json(user))
+    .catch(err => res.status(500).json(err))  
+}
 
 router
-  .get("/", (req, res) => {
-    User.find().then(users => res.json(users))
-  })
-  .get("/:id", (req, res) => {
-    User.findOne({id: req.params.id}).then(user => res.json(user))
-  })
+  .get("/", index)
+  .get("/:id", show)
+  .post("/", create)
 
 module.exports = router
